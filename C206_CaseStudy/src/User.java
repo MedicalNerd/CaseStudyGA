9643c528105ce20c383554a09b8f9ef0acb7f715
MedicalNerd <22021421@myrp.edu.sg>
2023-08-10 00:42:20 +0800

    YL doing add users

---
+++ C206_CaseStudy/src/User.java
@@ -0,0 +1,26 @@
+/*
+ * I declare that this code was written by me. 
+ * I do not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name: naoto
+ * Student ID: {type your id here}
+ * Class: {type your class here}
+ * Date/Time created: Wednesday 09-08-2023 21:48
+ */
+
+/**
+ * @author naoto
+ *
+ */
+public class User {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
