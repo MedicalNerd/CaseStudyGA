d73cb71aee56aa1a400c9d1f2a09cfa24805e9b5
SharulIman <22018526@myrp.edu.sg>
2023-08-09 19:28:30 +0800

    Fix?

--- C206_CaseStudy/src/Action.java
+++ C206_CaseStudy/src/Action.java
@@ -1,4 +1,33 @@
 
-public class Action {
-
+public class Action extends RenovationServices {
+    
+    private String recipientName;
+    private boolean status;
+    
+    public Action(String assetTag, String recipientName, boolean status) {
+        super(assetTag, "");  // Call the constructor of the superclass with assetTag and an empty serviceName
+        this.recipientName = recipientName;
+        this.status = status;
+    }
+    
+    public String getRecipientName() {
+        return recipientName;
+    }
+    
+    public void setRecipientName(String recipientName) {
+        this.recipientName = recipientName;
+    }
+    
+    public boolean getStatus() {
+        return status;
+    }
+    
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString() + String.format("\nRecipient Name: " + recipientName + "\nStatus: " + status);
+    }
 }
--- C206_CaseStudy/src/RenovationServices.java
+++ C206_CaseStudy/src/RenovationServices.java
@@ -1,4 +1,70 @@
 
 public class RenovationServices {
 
+	private String assetTag;
+	private String serviceName;
+	private String serviceDescription;
+	private String contactHours;
+	private boolean isAvailable;
+
+	public RenovationServices(String assetTag, String serviceName, String serviceDescription, String contactHours,
+			boolean isAvailable) {
+		this.assetTag = assetTag;
+		this.serviceName = serviceName;
+		this.serviceDescription = serviceDescription;
+		this.contactHours = contactHours;
+		this.isAvailable = isAvailable;
+	}
+
+	public RenovationServices(String assetTag, String serviceName) {
+		this.assetTag = assetTag;
+		this.serviceName = serviceName;
+	}
+
+	public String toString() {
+		String output = String.format("Asset Tag: " + assetTag + " \n Name: " + serviceName + " \n Description: "
+				+ serviceDescription + " \n Contact Hours " + contactHours + " \n Is Available: " + isAvailable);
+		return output;
+	}
+
+	public String getAssetTag() {
+		return assetTag;
+	}
+
+	public String getServiceName() {
+		return serviceName;
+	}
+
+	public String getDescription() {
+		return serviceDescription;
+	}
+
+	public String getContactHours() {
+		return contactHours;
+	}
+
+	public boolean getAvailability() {
+		return isAvailable;
+	}
+
+	public void setAssetTag(String assetTag) {
+		this.assetTag = assetTag;
+	}
+
+	public void setServiceName(String serviceName) {
+		this.serviceName = serviceName;
+	}
+
+	public void setServiceDescription(String serviceDescription) {
+		this.serviceDescription = serviceDescription;
+	}
+
+	public void setContactHours(String contactHours) {
+		this.contactHours = contactHours;
+	}
+
+	public void setAvailability(boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+
 }
