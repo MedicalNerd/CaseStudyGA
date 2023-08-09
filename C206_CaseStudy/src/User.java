a887813a77987e55929e5f757a91d5b1bc34ae15
MedicalNerd <22021421@myrp.edu.sg>
2023-08-10 00:46:46 +0800

    YL did this for Users

--- C206_CaseStudy/src/User.java
+++ C206_CaseStudy/src/User.java
@@ -13,14 +13,44 @@
  * @author naoto
  *
  */
+
 public class User {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private String username;
+	private String password;
+	private String email;
+	public User(String username,String password,String email){
+		this.username = username;
+		this.password = password;
+		this.email = email;
 	}
+	public String toString() {
+		
+		// Write your codes here
+		String UserInfo = String.format("%-10s %-30s %-10s %-10s", username,
+				password, 
+				email);
+		
+		return UserInfo;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 
 }
