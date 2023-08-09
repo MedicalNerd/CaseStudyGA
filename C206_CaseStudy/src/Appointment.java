6814ee002a351f2a3e072eff295fe694b886e782
Derrick888 <22011505@myrp.edu.sg>
2023-08-09 21:50:07 +0800

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/MedicalNerd/CaseStudyGA.git

--- C206_CaseStudy/src/Appointment.java
+++ C206_CaseStudy/src/Appointment.java
@@ -1,32 +1,40 @@
 
-public class Appointment {
+public class Appointment extends Action {
 	String date;
 	String time;
 	String additional_details;
-	
-	public Appointment(String date, String time, String additional_details) {
+
+	public Appointment(String assetTag, String recipientName,boolean status, String date, String time, String additional_details) {
+		super(assetTag, recipientName, status);
 		  this.date = date;
 		  this.time = time;
 		  this.additional_details = additional_details;  
 		}
 
+ 
+
 		public String getDate() {
 		  return date;
 		}
+
+ 
 
 		public String getTime() {
 		  return time;
 		}
 
+ 
+
 		public String getAdditional_details() {
 		  return additional_details;
 		}
 
-		public String toString() {
-		  String output = super.toString();
-		  output = String.format("Date: " + getDate() + "\n Time: " + getTime() + "\n Additional Details: " + getAdditional_details());
-		  
-		  return output;
+ 
 
-        }
-}
+	    @Override
+	    public String toString() {
+	        String output = super.toString();
+	        output += String.format("\nDate: %s\nTime: %s\nAdditional Details: %s", getDate(), getTime(), getAdditional_details());
+	        return output;
+	    }
+	}
