4e97a75147dccd25aca270315dabe0d8182f85a5
Derrick888 <22011505@myrp.edu.sg>
2023-08-10 19:49:28 +0800

    Added test case for add appointment and added line 27-29 and line 43-44

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -16,14 +16,23 @@ public class C206_CaseStudyTest {
 	private RenovationServices sa2;
 	private ArrayList<RenovationServices> serviceList;
 	
+	private Appointment ap1;
+	private Appointment ap2;
+	private ArrayList<Appointment> appointmentList;
+	
 
 
 	@Before
 	public void setUp() throws Exception {
+		ServiceProviderList = new ArrayList<ServiceProvider>();
+		serviceList = new ArrayList<RenovationServices>();
+		appointmentList = new  ArrayList<Appointment>();
+		
 		sp1 = new ServiceProvider("Johns House Renos", "09:00 to 18:00");
 		sp2 = new ServiceProvider("Daisy Lawn Renos", "09:00 to 18:00");
 		ServiceProviderList.add(sp1);
 		ServiceProviderList.add(sp2);
+		
 		sa1 = new RenovationServices("SA1", "Johns House Renos", "House Renovation",
 				"Specialises in House Renovation", "09:00 to 18:00", true);
 		sa2 = new RenovationServices("SA2", "Daisy Lawn Renos", "Lawn Renovation",
@@ -31,6 +40,10 @@ public class C206_CaseStudyTest {
 		serviceList.add(sa1);
 		serviceList.add(sa2);
 
+		ap1 = new Appointment("AP1", "Kafka", "Pending", "2023-07-25", "10.00", "Johns House Renos");
+		ap2 = new Appointment("AP2", "Mary", "Pending", "2023-07-25", "11.00", "Daisy Lawn Renos");
+		
+		
 	}
 	
 
@@ -93,5 +106,29 @@ public class C206_CaseStudyTest {
         isDeleted = C206_CaseStudy.deleteServiceProvider(ServiceProviderList);
         assertFalse(isDeleted);
     }
+	
+	@Test
+	public void testAddAppointment() { //Derrick
+		assertNotNull("Test if there is valid Appointment arraylist to add to", appointmentList);
+		
+		appointmentList.add(ap1);
+		C206_CaseStudy.createAppointment(appointmentList, ap1);	
+		assertEquals("Test that the Appointment arraylist size is 1.", 1, appointmentList.size());
+		assertSame("Test that Appointment is added to the end of the list.", ap1, appointmentList.get(0));
+		
+		appointmentList.add(ap2);
+		C206_CaseStudy.createAppointment(appointmentList, ap2);
+		assertEquals("Test that the Chromebook arraylist size is now 2.", 2, appointmentList.size());
+		assertSame("Test that Chromebook is added to the end of the list.", ap2, appointmentList.get(1));
+		
+		C206_CaseStudy.createAppointment(appointmentList, ap2);
+		assertEquals("Test that the Appointment arraylist size is unchange.", 2, appointmentList.size());
+		
+		Appointment ap_missing = new Appointment("AP3", "", "Pending", "2023-07-26", "11.00", "Johns House Renos");
+		C206_CaseStudy.createAppointment(appointmentList, ap_missing);
+		assertEquals("Test that the Chromebook arraylist size is unchange.", 2, appointmentList.size());
+		
+		
+	}
 
 }
