f6c78d48cadac9600fabda8dc2ce241f157bcaf5
SharulIman <22018526@myrp.edu.sg>
2023-08-10 15:00:04 +0800

    Updated Testcases for Service Provider and Service

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,23 +1,97 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class C206_CaseStudyTest {
+	
+	private ServiceProvider sp1;
+	private ServiceProvider sp2;
+	private ArrayList<ServiceProvider> ServiceProviderList;
+	
+	private RenovationServices sa1;
+	private RenovationServices sa2;
+	private ArrayList<RenovationServices> serviceList;
+	
+
 
 	@Before
 	public void setUp() throws Exception {
+		sp1 = new ServiceProvider("Johns House Renos", "09:00 to 18:00");
+		sp2 = new ServiceProvider("Daisy Lawn Renos", "09:00 to 18:00");
+		ServiceProviderList.add(sp1);
+		ServiceProviderList.add(sp2);
+		sa1 = new RenovationServices("SA1", "Johns House Renos", "House Renovation",
+				"Specialises in House Renovation", "09:00 to 18:00", true);
+		sa2 = new RenovationServices("SA2", "Daisy Lawn Renos", "Lawn Renovation",
+				"Specialises in Lawn Renovation", "08:00 to 18:00", true);
+		serviceList.add(sa1);
+		serviceList.add(sa2);
+
 	}
+	
 
 	@After
 	public void tearDown() throws Exception {
 	}
+	
+	@Test
+    public void testAddService() {
+    	//Add a service as an example.
+    	RenovationServices newService = new RenovationServices("SA3", "New Service Provider", "New Service Name",
+    			"New Service Description", "10:00 to 20:00", true);
+        C206_CaseStudy.addService(serviceList, newService);
+        //Check if the number of services is now 3, as there were 2 already existing services before the new one was added.
+        assertEquals(3, serviceList.size());
+        //Check if the newly added service is equal to the information that was inputted.
+        assertEquals(newService, serviceList.get(2));
+	}
 
 	@Test
-	public void c206_test() {
-		//fail("Not yet implemented"); 
-		assertTrue("C206_CaseStudy_SampleTest ",true);
+    public void testDeleteService() {
+    	//Delete a service as an example.
+        boolean isDeleted = C206_CaseStudy.deleteService(serviceList, "SA1");
+        assertTrue(isDeleted);
+        //Check if there is only one service left in the list, as we have deleted 1 out of 2 of the services.
+        assertEquals(1, serviceList.size());
+        //Check if the service is no longer existing.
+        isDeleted = C206_CaseStudy.deleteService(serviceList, "SA1");
+        assertFalse(isDeleted);
+    }
+
+	@Test
+	public void testViewAllServiceProvider() {
+	    // Create the expected output that is supposed to be printed out
+	    String expectedOutput = String.format("%-20s %-40s\n", "SERVICE PROVIDER", "CONTACT HOURS");
+	    expectedOutput += String.format("%-20s %-40s\n", "Johns House Renos", "09:00 to 18:00");
+	    expectedOutput += String.format("%-20s %-40s\n", "Daisy Lawn Renos", "09:00 to 18:00");	
+	    assertEquals(expectedOutput, C206_CaseStudy.retrieveAllServiceProvider(ServiceProviderList));
 	}
+
+	@Test
+    public void testAddServiceProvider() {
+    	//Add a service as an example.
+    	ServiceProvider newSP = new ServiceProvider("Service Provider3", "4:00 to 18:00");
+        C206_CaseStudy.addServiceProvider(ServiceProviderList, newSP);
+        //Check if the number of services is now 3, as there was 2 already existing service before the new one was added.
+        assertEquals(3, ServiceProviderList.size());
+        //Check if the newly added service is equal to the information that was inputed.
+        assertEquals(newSP, ServiceProviderList.get(2));
+	}
+
+	@Test
+    public void testDeleteServiceProvider() {
+    	//Delete a service provider as an example.
+        boolean isDeleted = C206_CaseStudy.deleteServiceProvider(ServiceProviderList);
+        assertTrue(isDeleted);
+        //Check if there is only one service provider left in the list, as we have deleted 1 out of 2 of the services.
+        assertEquals(1, ServiceProviderList.size());
+        //Check if the service provider is no longer existing.
+        isDeleted = C206_CaseStudy.deleteServiceProvider(ServiceProviderList);
+        assertFalse(isDeleted);
+    }
 
 }
