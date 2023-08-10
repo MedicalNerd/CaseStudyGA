c287ed579a4308e15493cab3ce355d41558133c0
SharulIman <22018526@myrp.edu.sg>
2023-08-10 15:12:41 +0800

    Fixed Request and Quote

--- C206_CaseStudy/src/Request.java
+++ C206_CaseStudy/src/Request.java
@@ -1,4 +1,35 @@
 
-public class Request { 
+public class Request extends Action {
 
+    private int contactNumber;
+    private String description;
+
+    public Request(String assetTag, String recipientName, String status, int contactNumber, String description) {
+        super(assetTag, recipientName, status);
+        this.contactNumber = contactNumber;
+        this.description = description;
+    }
+
+    public int getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(int contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+               String.format("\nContact Number: %d\nDescription: %s", contactNumber, description);
+    }
 }
+
